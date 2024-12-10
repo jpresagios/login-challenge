9626740a414530ae409add496de04e8d6c321c1d
Junior <junior.presagios@gmail.com>
2024-12-10 23:37:39 +0200

    Showcase protected route

---
+++ client/src/app/experimental/page.tsx
@@ -0,0 +1,10 @@
+const Experimental = () => {
+  return (
+    <div>
+      You are seeing this page because you are currently logged in and
+      authenticated. 
+    </div>
+  );
+};
+
+export default Experimental;
--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,21 +1,14 @@
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
-const protectedRoutes = ["/dashboard"];
-const publicRoutes = ["/"];
+const protectedRoutes = ["/experimental"];
 
 export default async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
   const isProtectedRoute = protectedRoutes.includes(path);
-  const isPublicRoute = publicRoutes.includes(path);
-
   const session = (await cookies()).get("session")?.value;
 
   if (isProtectedRoute && !session) {
     return NextResponse.redirect(new URL("/", req.nextUrl));
-  }
-
-  if (isPublicRoute && session) {
-    return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
   }
 
   return NextResponse.next();
