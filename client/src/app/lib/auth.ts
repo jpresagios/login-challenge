cd0aac503b112157e905e23c217cd0b23e0138f7
Junior <junior.presagios@gmail.com>
2024-12-10 20:45:01 +0200

    lib to handle auth flow

---
+++ client/src/app/lib/auth.ts
@@ -0,0 +1,21 @@
+import "server-only";
+
+import { cookies } from "next/headers";
+
+export async function createSession(accessToken: string) {
+  const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+
+  const cookieStore = await cookies()
+  cookieStore.set("session", accessToken, {
+    httpOnly: true,
+    secure: true,
+    expires: expiresAt,
+    path: '/',
+  });
+}
+
+export async function deleteSession() {
+  console.log("YEAH")
+  const cookieStore = await cookies()
+  cookieStore.delete('session')
+}
