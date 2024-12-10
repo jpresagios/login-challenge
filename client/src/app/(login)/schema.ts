0b17acb6f3ba4a44798dfa695c5fcd44327891ca
Junior <junior.presagios@gmail.com>
2024-12-10 20:44:43 +0200

    set schema validation for user login

---
+++ client/src/app/(login)/schema.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+
+const schema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters long")
+    .refine(
+      (value) => /^(?=.*[a-zA-Z])(?=.*\d).+$/.test(value ?? ""),
+      "Password must contain at least one letter and one digit"
+    ),
+});
+
+export const another = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z
+    .string()
+    .min(10, "Password must be at least 6 characters long")
+    .refine(
+      (value) => /^(?=.*[a-zA-Z])(?=.*\d).+$/.test(value ?? ""),
+      "Password must contain at least one letter and one digit"
+    ),
+});
+
+export type FormSchemaData = z.infer<typeof schema>;
+export { schema };
