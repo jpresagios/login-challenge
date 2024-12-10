7d346a520d9e1846ec44c6c8efdfe188e810013d
Junior <junior.presagios@gmail.com>
2024-12-10 20:41:27 +0200

    Add custom Tailwind colors from Figma

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -11,7 +11,16 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        purple: '#F0EDFFCC',
+        darkpurple: '#7749F8',
+        graylight: '#343A40'
       },
+      backgroundImage: {
+        'purple-gradient': 'linear-gradient(99.78deg, #9181F4 -5.85%, #5038ED 109.55%)',
+      },
+      boxShadow: {
+        'shadow-button': 'box-shadow: 0px 8px 21px 0px #00000029'
+      }
     },
   },
   plugins: [],
